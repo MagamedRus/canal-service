c7515100a94f2af63fb74564c988757def15702d
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-08 07:26:50 +0500

    init pages

---
+++ src/pages/AuthPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AuthPage(props) {
+  
+
+  return (
+    <>
+      <div />
+    </>
+  );
+}
+
+export default AuthPage;
---
+++ src/pages/PublishesPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+function PublishesPage(props) {
+  return (
+    <div>
+      <Header isExitBtn={true} username={'asdasd'} />
+    </div>
+  );
+}
+
+export default PublishesPage;
