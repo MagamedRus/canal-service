4607bcb15ed8af80a75c8ce53f919d4849aa376f
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-09 17:39:58 +0500

    new styled component TextInput

---
+++ src/styledComponents/TextInput.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { blueWater } from "../constants/styles/colors";
+
+const TextInput = styled.input.attrs({
+  type: "text",
+})`
+  height: 37px;
+  width: 285px;
+  background-color: #d9d9d9;
+  padding: 0 5px;
+  border: 4px solid ${blueWater};
+  border-radius: 10px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 16px;
+  line-height: 19px;
+  outline: none;
+`;
+
+export default TextInput;
