2dcc40bf914c638989b227040fd03e71430c3275
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-11 23:27:26 +0500

    delete unusing scope ErrorIndificator

--- src/components/ErrorIndificator.js
+++ src/components/ErrorIndificator.js
@@ -3,14 +3,12 @@ import styled, { css } from "styled-components";
 import ErrorBorderText from "../styledComponents/ErrorBorderText";
 import ErrorText from "../styledComponents/ErrorText";
 
-const ErrorIndificator = ({ isShow, styles }) => {
-  return (
-    <Container isShow={isShow} styles={styles}>
-      <ErrorBorderText>!</ErrorBorderText>
-      <ErrorText>Bad auth: Invalid login or password!</ErrorText>
-    </Container>
-  );
-};
+const ErrorIndificator = ({ isShow, styles }) => (
+  <Container isShow={isShow} styles={styles}>
+    <ErrorBorderText>!</ErrorBorderText>
+    <ErrorText>Bad auth: Invalid login or password!</ErrorText>
+  </Container>
+);
 
 export default ErrorIndificator;
 
