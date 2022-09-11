c1f2700d1c0861e02c6c4eea35b73a328f2e2928
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-11 19:49:56 +0500

    exit on press exit btn

--- src/components/UsernnameWithExit.js
+++ src/components/UsernnameWithExit.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import ExitButton from "../styledComponents/ExitButton";
 import Username from "../styledComponents/Username";
+import { resetAuth } from "../store/action-creators/auth";
 
-const UsernameWithExit = ({ username }) => (
-  <Container>
-    <Username>{username}</Username>
-    <ExitButton />
-  </Container>
-);
+const UsernameWithExit = ({ username }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const exit = () => {
+    dispatch(resetAuth());
+    navigate("auth");
+  };
+
+  return (
+    <Container>
+      <Username>{username}</Username>
+      <ExitButton onClick={exit} />
+    </Container>
+  );
+};
 
 export default UsernameWithExit;
 
