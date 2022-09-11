e1dec3f2b953638391269439493339de91b2ae9d
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-11 20:51:09 +0500

    draw PublishCards in page

--- src/pages/PublishesPage.js
+++ src/pages/PublishesPage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import PublishCard from "../components/PublishCard";
+import { getPosts } from "../controllers/post";
 
 function PublishesPage() {
+  const [posts, setPosts] = useState([]);
+  const userData = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    (async () => {
+      const reqPostsData = await getPosts(userData.id);
+      reqPostsData.status === 200 && setPosts(reqPostsData.posts);
+    })();
+  }, [userData.id]);
+
   return (
     <Container>
-      <PublishCard />
+      <PublishCards>
+        {posts.map((post) => (
+          <PublishCard key={post.id} publishData={post} />
+        ))}
+      </PublishCards>
     </Container>
   );
 }
@@ -17,4 +33,11 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   padding: 138px 0px 90px 0px;
+`;
+
+const PublishCards = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-auto-rows: 1fr 1fr;
+  gap: 20px;
 `;
