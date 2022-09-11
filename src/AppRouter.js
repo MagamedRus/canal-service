239d0d1d5c8715157ca152a558036accb6a5470a
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-12 01:03:34 +0500

    gh-pages prepare

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import { basePath, authPath } from "./constants/routePath";
 import AuthPage from "./pages/AuthPage";
 import PublishesPage from "./pages/PublishesPage";
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Header />
     <Routes>
       <Route path={authPath} element={<AuthPage />} />
       <Route path={basePath} element={<PublishesPage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default AppRouter;
