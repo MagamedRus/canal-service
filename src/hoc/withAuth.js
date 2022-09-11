12efae40e66b795a7aa2402b744ef75050d03aa9
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-11 23:25:44 +0500

    delete unusing console.log and scope

--- src/hoc/withAuth.js
+++ src/hoc/withAuth.js
@@ -2,17 +2,14 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-const withAuth = (WrappedComponent) => {
-  return () => {
-    const navigate = useNavigate();
-    const authData = useSelector((state) => state.auth);
-    useEffect(() => {
-      authData.id === null && navigate("auth");
-      console.log(authData);
-    }, [authData, navigate]);
+const withAuth = (WrappedComponent) => () => {
+  const navigate = useNavigate();
+  const authData = useSelector((state) => state.auth);
+  useEffect(() => {
+    authData.id === null && navigate("auth");
+  }, [authData, navigate]);
 
-    return <WrappedComponent />;
-  };
+  return <WrappedComponent />;
 };
 
 export default withAuth;
