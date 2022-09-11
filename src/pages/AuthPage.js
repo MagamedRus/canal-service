d10a2e848e99120aee639ba7b0366fd8b1528b76
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-11 19:30:11 +0500

    provide auth request to Auth Page

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -5,22 +5,27 @@ import styled from "styled-components";
 import LoginCard from "../components/LoginCard";
 import { basePath } from "../constants/routePath";
 import { AuthActions } from "../constants/store/actionTypes";
+import { authUser } from "../controllers/user";
 
 function AuthPage() {
   const [isError, setIsError] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const sumbit = () => {
-    const userData = {}; //Todo: make request here
-    !userData && setIsError(true);
-    dispatch({ type: AuthActions.SET_AUTH_DATA, payload: userData });
-    navigate(basePath);
+  const submit = async (authData) => {
+    const reqAuthData = await authUser(authData);
+    if (reqAuthData.status !== 200) {
+      setIsError(true);
+    } else {
+      const userData = reqAuthData.userData;
+      dispatch({ type: AuthActions.SET_AUTH_DATA, payload: userData });
+      navigate(basePath);
+    }
   };
 
   return (
     <Container>
-      <LoginCard sumbit={sumbit} isBadAuth={isError} />
+      <LoginCard submit={submit} isBadAuth={isError} />
     </Container>
   );
 }
